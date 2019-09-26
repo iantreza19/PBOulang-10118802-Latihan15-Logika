8c7d3f610a644525f0f0c10ae425563572c0af3d
iantreza19 <55836534+iantreza19@users.noreply.github.com>
2019-09-26 20:56:09 +0700

    Add files via upload

---
+++ Latihan15/src/PBOUlang10118802Latihan15.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author IantReza
+ */
+public class PBOUlang10118802Latihan15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = "+ (a && b));
+        System.out.println("a || b = "+ (a || b));
+        System.out.println("!a = "+ !a);
+        System.out.println("!(a && b) = "+ !(a && b));
+    }
+    
+}
